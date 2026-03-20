7570d598309b063ac76e616c74781146a2f97b22
Joshua Anthony Aurellano <jaaurellano@gbox.ncf.edu.ph>
2026-03-20 12:14:27 +0800

    Added Upload and Download results component for reusable UI

---
+++ src/components/downloadResultsComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+function DownloadResultsComponent() {
+  return (
+    <div>
+        <span style={{fontWeight:'bold'}}>Download Results</span>
+            <Card style={{height:'200px'}}>
+
+            </Card>
+    </div>
+  )
+}
+
+export default DownloadResultsComponent
